7eb40f2f7b082647adebe63d09aa3d5221a3cdc7
Ivaylo Getov <ivaylo.getov@gmail.com>
2015-08-17 02:20:06 -0700

    add antigen install to setup script

--- setup-a-new-machine.sh
+++ setup-a-new-machine.sh
@@ -92,6 +92,9 @@ fi
 ##############################################################################################################
 ### homebrew!
 
+git clone https://github.com/zsh-users/antigen .zsh-antigen/antigen
+chsh -s /bin/zsh
+
 # (if your maching has /usr/local locked down (like google's), you can do this to place everything in ~/.homebrew
 mkdir $HOME/.homebrew && curl -L https://github.com/mxcl/homebrew/tarball/master | tar xz --strip 1 -C $HOME/.homebrew
 export PATH=$HOME/.homebrew/bin:$HOME/.homebrew/sbin:$PATH
