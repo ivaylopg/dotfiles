e564f0df22e9b6309d27951f04785d88e5348674
Ivaylo Getov <ivaylo.getov@gmail.com>
2015-08-25 01:49:22 -0400

    add git-friendly install script

--- 05_misc.sh
+++ 05_misc.sh
@@ -3,5 +3,8 @@
 #install wifi-password
 curl -L https://raw.github.com/rauchg/wifi-password/master/wifi-password.sh -o /usr/local/bin/wifi-password && chmod +x /usr/local/bin/wifi-password
 
+#install git-friendly
+bash < <( curl https://raw.githubusercontent.com/jamiew/git-friendly/master/install.sh)
+
 #set up Colossal Cave Adventure 2.5 (430pts)
 mkdir ~/.adventure && cp bin/adventure/* ~/.adventure/
