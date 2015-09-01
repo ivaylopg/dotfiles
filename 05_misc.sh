eea1177a3425d5e8a65ac7f1482dcdcf191ef6fb
Ivaylo Getov <ivaylo.getov@gmail.com>
2015-09-01 02:42:08 +0100

    add font install script

--- 05_misc.sh
+++ 05_misc.sh
@@ -6,5 +6,12 @@ curl -L https://raw.github.com/rauchg/wifi-password/master/wifi-password.sh -o /
 #install git-friendly
 bash < <( curl https://raw.githubusercontent.com/jamiew/git-friendly/master/install.sh)
 
+#install CamingoCode Font
+mkdir $HOME/Desktop/camingo
+cd $HOME/Desktop/camingo
+curl -O http://janfromm.de/_data/downloads/CamingoCode-v1.0.zip
+unzip *
+open CamingoCode-*.ttf
+
 #set up Colossal Cave Adventure 2.5 (430pts)
 mkdir ~/.adventure && cp bin/adventure/* ~/.adventure/
