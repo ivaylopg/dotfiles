0a18a7a5d5e956f5047f83545d2229edb3c210ca
Ivaylo Getov <ivaylo.getov@gmail.com>
2015-09-17 19:03:24 -0700

    fix some syntax

--- 05_misc.sh
+++ 05_misc.sh
@@ -4,7 +4,7 @@
 curl -L https://raw.github.com/rauchg/wifi-password/master/wifi-password.sh -o /usr/local/bin/wifi-password && chmod +x /usr/local/bin/wifi-password
 
 #install git-friendly
-bash < <( curl https://raw.githubusercontent.com/jamiew/git-friendly/master/install.sh)
+curl -s https://raw.githubusercontent.com/jamiew/git-friendly/master/install.sh | bash
 
 #install CamingoCode Font
 mkdir $HOME/Desktop/camingo
