6ccfa015863c1b77c520c5629c7a8273c10c2387
Ivaylo Getov <ivaylo.getov@gmail.com>
2015-09-26 17:13:02 -0700

    add aws cli install script and make more verbose

--- 05_misc.sh
+++ 05_misc.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
 #install wifi-password
+echo "##### INSTALLING wifi-password #####"
 curl -L https://raw.github.com/rauchg/wifi-password/master/wifi-password.sh -o /usr/local/bin/wifi-password && chmod +x /usr/local/bin/wifi-password
 
 #install git-friendly
+echo "##### INSTALLING git-friendly #####"
 curl -s https://raw.githubusercontent.com/jamiew/git-friendly/master/install.sh | bash
 
 #install CamingoCode Font
+echo "##### INSTALLING CamingoCode Font #####"
 mkdir $HOME/Desktop/camingo
 cd $HOME/Desktop/camingo
 curl -O http://janfromm.de/_data/downloads/CamingoCode-v1.0.zip
@@ -16,4 +19,12 @@ cd ../
 mv camingo ~/.Trash
 
 #set up Colossal Cave Adventure 2.5 (430pts)
+echo "##### INSTALLING Colossal Cave Adventure #####"
 mkdir ~/.adventure && cp $(pwd)/bin/adventure/* ~/.adventure/
+
+#install aws cli
+echo "##### INSTALLING AWS CLI #####"
+cd $HOME/Desktop
+curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+unzip awscli-bundle.zip
+sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
