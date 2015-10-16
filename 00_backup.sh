9fc14ea764a5343befdb2d0fbd3f135f9aee56e8
Ivaylo Getov <ivaylo.getov@gmail.com>
2015-10-15 23:20:50 -0700

    cleanup

--- 00_backup.sh
+++ 00_backup.sh
@@ -7,16 +7,15 @@ mkdir -p ~/migration/ST
 cd ~/migration
 
 # what is worth reinstalling?
-brew leaves         > brew-list.txt    # all top-level brew installs
-brew cask list      > cask-list.txt
+brew leaves             > brew-list.txt    # all top-level brew installs
+brew cask list          > cask-list.txt
 npm list -g --depth=0   > npm-g-list.txt
 
-cp ~/.z ~/migration/home
 cp -R ~/.ssh ~/migration/home
+cp -R ~/.zsh-antigen ~/migration/home
 cp /Library/Preferences/SystemConfiguration/com.apple.airport.preferences.plist ~/migration  # wifi
 cp -R ~/Library/Services ~/migration # automator stuff
 cp ~/.bash_history ~/migration # back it up for fun?
 cp ~/.zsh_history ~/migration # back it up for fun?
 #cp ~/.gitconfig.local ~/migration
-#cp ~/.z ~/migration # z history file.
 cp -r  ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/ ~/migration/ST
