6be1c034076185c1dead7cf5fce5ae007e9e3b4a
Ivaylo Getov <ivaylo.getov@gmail.com>
2015-10-18 23:58:19 -0700

    add confirmations to 05_misc script

--- 05_misc.sh
+++ 05_misc.sh
@@ -1,27 +1,63 @@
 #!/bin/sh
 
-#install alcatraz xcode package manager
-curl -fsSL https://raw.githubusercontent.com/supermarin/Alcatraz/deploy/Scripts/install.sh | sh
-#rm -rf ~/Library/Application\ Support/Developer/Shared/Xcode/Plug-ins/Alcatraz.xcplugin
-#rm -rf ~/Library/Application\ Support/Developer/Shared/Xcode/Plug-ins/Alcatraz.xcplugin
+source 99_utils.sh
 
-#install wifi-password
-echo "##### INSTALLING wifi-password #####"
-curl -L https://raw.github.com/rauchg/wifi-password/master/wifi-password.sh -o /usr/local/bin/wifi-password && chmod +x /usr/local/bin/wifi-password
+ask_for_confirmation "Install Alcatraz (Xcode package manager)?"
+if answer_is_yes; then
+  print_info "Installing Alcatraz..."
+  curl -fsSL https://raw.githubusercontent.com/supermarin/Alcatraz/deploy/Scripts/install.sh | sh
+  #rm -rf ~/Library/Application\ Support/Developer/Shared/Xcode/Plug-ins/Alcatraz.xcplugin
+  #rm -rf ~/Library/Application\ Support/Developer/Shared/Xcode/Plug-ins/Alcatraz.xcplugin
+fi
 
-#install git-friendly
-echo "##### INSTALLING git-friendly #####"
-curl -s https://raw.githubusercontent.com/jamiew/git-friendly/master/install.sh | bash
+ask_for_confirmation "Install wifi-password?"
+if answer_is_yes; then
+  print_info "Installing wifi-password"
+  curl -L https://raw.github.com/rauchg/wifi-password/master/wifi-password.sh -o /usr/local/bin/wifi-password && chmod +x /usr/local/bin/wifi-password
+fi
 
-#set up sublime text CLI
-sudo ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /usr/bin/subl
 
-#install aws cli
-echo "##### INSTALLING AWS CLI #####"
-cd $HOME/Desktop
-curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
-unzip awscli-bundle.zip
-sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
+ask_for_confirmation "Install git-friendly?"
+if answer_is_yes; then
+  print_info "Installing git-friendly"
+  curl -s https://raw.githubusercontent.com/jamiew/git-friendly/master/install.sh | bash
+fi
+
+
+ask_for_confirmation "Configure Sublime Text CLI?"
+if answer_is_yes; then
+  print_info "Setting up Sublime CLI"
+  sudo ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /usr/bin/subl
+fi
+
+
+ask_for_confirmation "Install AWS CLI?"
+if answer_is_yes; then
+  print_info "Installing AWS CLI"
+  cd $HOME/Desktop
+  curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+  unzip awscli-bundle.zip
+  sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
+fi
+
+
+ask_for_confirmation "Install Colossal Cave Adventure?"
+if answer_is_yes; then
+  #set up Colossal Cave Adventure 2.5 (430pts)
+  print_info "Installing Colossal Cave Adventure"
+  mkdir ~/.adventure && cp $(pwd)/bin/adventure/* ~/.adventure/
+fi
+
+ask_for_confirmation "Restore Mackup Settings?"
+if answer_is_yes; then
+  print_info "Restoring Mackup Settings"
+  mackup restore
+fi
+
+# ask_for_confirmation ""
+# if answer_is_yes; then
+# fi
+
 
 #install CamingoCode Font ( This is now done in brew cask)
 # echo "##### INSTALLING CamingoCode Font #####"
@@ -32,7 +68,3 @@ sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
 # open CamingoCode-*.ttf
 # cd ../
 # mv camingo ~/.Trash
-
-#set up Colossal Cave Adventure 2.5 (430pts)
-echo "##### INSTALLING Colossal Cave Adventure #####"
-mkdir ~/.adventure && cp $(pwd)/bin/adventure/* ~/.adventure/
